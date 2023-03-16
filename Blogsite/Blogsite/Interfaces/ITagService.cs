923765e2b9df567c01c238d178c64c5cb8d83484
saltionsatka <saltionsatka@gmail.com>
2023-03-17 00:54:07 +0100

    tag repository implemented commit

---
+++ Blogsite/Blogsite/Interfaces/ITagService.cs
@@ -0,0 +1,18 @@
+﻿using Blogsite.DTO_Models.RequestDtos;
+using Blogsite.DTO_Models;
+
+namespace Blogsite.Interfaces
+{
+    public interface ITagService
+    {
+        Task<List<TagDto>> GetTagsAsync();
+
+        Task<TagDto> GetTagByIdAsync(int id);
+
+        Task<TagDto?> CreateTagAsync(RequestTagDto requestTagDto);
+
+        Task<TagDto?> UpdateTagAsync(RequestTagDto requestTagDto);
+
+        Task<bool> DeleteTagAsync(int id);
+    }
+}
